1a5ad546a719d71d2aa67135b6d584d721cbb3b3
Sam Zhang <samzhangjy@outlook.com>
2022-05-15 09:22:01 +0800

    :bug: improved ux

--- components/Header.js
+++ components/Header.js
@@ -23,13 +23,25 @@ export default function Header() {
         <div className={style.spacerSubtitle} />
         <GridContainer cols={12} className={style.navigationContainer}>
           {config.links.map((link, index) => (
-            <GridCol key={index} colSpan={12 / config.links.length} md={3} sm={4}>
+            <GridCol
+              key={index}
+              colSpan={12 / config.links.length}
+              md={3}
+              sm={4}
+            >
               <a className={style.navigation} href={link.link}>
                 <h5 className={style.navigationText}>{link.text}</h5>
               </a>
             </GridCol>
           ))}
         </GridContainer>
+      </div>
+      <div className={style.mobileSlogan}>
+        <h4 className={style.mobileSloganItem}>
+          {config.site.slogan.map((slogan, index) => {
+            return <span key={index} className={style.mobileSloganSlug}>{slogan}.</span>;
+          })}
+        </h4>
       </div>
     </>
   );
