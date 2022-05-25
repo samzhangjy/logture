ce89210f8c595c07bc9b757e6a1b6c59a5be14d7
Sam Zhang <samzhangjy@outlook.com>
2022-05-25 21:24:07 +0800

    feat: added support for in-markdown html tags

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,6 +1,8 @@
 import MarkdownIt from "markdown-it/lib";
 
 export default async function markdownToHtml(markdown: string) {
-  const md = new MarkdownIt().use(require("markdown-it-highlightjs"));
+  const md = new MarkdownIt({
+    html: true,
+  }).use(require("markdown-it-highlightjs"));
   return md.render(markdown);
 }
