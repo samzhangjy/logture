9a42e184c4f4586d38e247b54f60d1d255095cc0
Sam Zhang <samzhangjy@outlook.com>
2022-06-07 09:30:28 +0800

    fix: used inline display for header navigation instead of grid

--- components/Header.tsx
+++ components/Header.tsx
@@ -21,20 +21,14 @@ export default function Header() {
         <div className={style.spacer} />
         <h4 className={style.subtitle}>{config.site.description}</h4>
         <div className={style.spacerSubtitle} />
-        <GridContainer cols={12} className={style.navigationContainer}>
+        <div className={style.navigationContainer}>
           {config.links.map((link, index) => (
-            <GridCol
-              key={index}
-              colSpan={12 / config.links.length}
-              md={3}
-              sm={4}
-            >
-              <a className={style.navigation} href={link.link}>
+              <a className={style.navigation} href={link.link} key={index}>
                 <h5 className={style.navigationText}>{link.text}</h5>
               </a>
-            </GridCol>
+            // </GridCol>
           ))}
-        </GridContainer>
+        </div>
       </div>
       <div className={style.mobileSlogan}>
         <h4 className={style.mobileSloganItem}>
